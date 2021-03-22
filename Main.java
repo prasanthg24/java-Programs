de42436eba89fbbe983b868d88e41831ded52fa5
PRASANTH G <prasanth287471@gmail.com>
2021-03-22 03:38:33 +0000

    first commit

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
   public static void main(String[] args)
 	 {
+		 byte age=30;
+		 int viewsCount=2_12_2123;
+		 double price=12.3344F;
+		 
+
     System.out.println("Hello world !!!");
   }
 }
