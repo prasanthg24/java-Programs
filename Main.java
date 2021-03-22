2313e6173448f9b012e5b8e7c5a9072ec2583e06
PRASANTH G <prasanth287471@gmail.com>
2021-03-22 03:44:38 +0000

    second commit

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
+import java.util.Date;
 class Main {
   public static void main(String[] args)
 	 {
-		 byte age=30;
-		 int viewsCount=2_12_2123;
-		 double price=12.3344F;
-		 
-
-    System.out.println("Hello world !!!");
+		Date now =new Date();
+		now.getTime();
+		System.out.println(now);
   }
 }
 
